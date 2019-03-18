e8bbbd602b0d9cb67e6ecaab0c3ff9311cc7a784
XavierGougat <xaviergougat@gmail.com>
2019-03-18 15:57:53 +0100

    init.sh tables & load

---
+++ SH/init_ref.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+. /home/metiadm/cron_setenv.sh
+
+FILE_LOG='LOG/INIT_REF.log'
+
+echo "Creation des tables temporaires de referentiel : `date`" > $FILE_LOG
+sqlplus -S -L MBCEN/$PWD_USER@$ORACLE_SERVICE @SQL/INIT/init_ref.sql >> $FILE_LOG
+
+echo "Alimentation des tables temporaires avec les donnees du dump : `date`" >> $FILE_LOG
+
+echo "Fournisseurs : `date`" >> $FILE_LOG
+sh load_fournisseur.sh >> $FILE_LOG
+
+echo "Produits : `date`" >> $FILE_LOG
+sh load_produit.sh >> $FILE_LOG
+
+echo "Libelles produits : `date`" >> $FILE_LOG
+sh load_lib_produit.sh >> $FILE_LOG
+
+echo "Code a barres : `date`" >> $FILE_LOG
+sh load_code_barre.sh >> $FILE_LOG
+
+echo "Affectations produits fournisseur : `date`" >> $FILE_LOG
+sh load_produit_four.sh >> $FILE_LOG
+
+exit 0

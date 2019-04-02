cd21d44aab6cbe08d654abe75758078a7cc3aba4
XavierGougat <xaviergougat@gmail.com>
2019-04-02 17:33:19 +0200

    post_rar script

--- SQL/MAIN/POST_RAR.sql
+++ SQL/MAIN/POST_RAR.sql
@@ -6,6 +6,11 @@ BEGIN
         select crt_noart from mgcrt where crt_cdcaract = 'RDD_CAS' and crt_znvaleur in ('4','5')
     );
     commit;
+
+    insert into MGMSA (select 'M', msa_noart, 'A ', '  ' from mgmsa);
+    exception
+    when dup_val_on_index then null;
+    commit;
 END;
 /
 quit
